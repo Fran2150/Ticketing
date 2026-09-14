bb8541369fe7617311bfe030d6623b4c8ef9c2e5
Fran2150 <faadeleons05@gmail.com>
2026-09-14 05:45:43 -0600

    Add files via upload

---
+++ Ticketing/GestorTicket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SoporteTicket.Logica { 
+    public class GestorTicket
+    {
+        public List<Ticket> lstTecnicos { get; set; }
+        public List<Solicitante> lstSolicitantes { get; set; }
+        public List<Ticket> lstTickets { get; set; }
+        public GestorTicket()
+        {
+            lstSolicitantes = new List<Solicitante>();
+            lstTecnicos = new List<Tecnico>();
+            lstTickets = new List<Ticket>();
+        }
+        public Ticket CrearTicket(int intNumero, string Asunto, string Descripcion, string strCategoria, string strPrioridad, Solicitante objSolicitante)
+        {
+
+        //Validaciones
+
+            Ticket objTicket = new Ticket(intNumero, Asunto, Descripcion, strCategoria, strPrioridad, objSolicitante);
+            objTicket.asignarTecnico(objTecnico);
+            lstTickets.add(objTicket);
+            retur objticket;
+        }
+        public Ticket BuscarTicket(int intTicket)
+        {
+        Ticket objTicket;
+        lstTickets.Find
+            (testc => t.intNumero == intTicket);
+            if (objTicket == null)
+            {
+            Console.WriteLine("Ticket NO existe");
+                return objTicket;
+            }
+        }
+    } 
+}
+    
---
+++ Ticketing/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicketsSoporte.Logica
+{
+    public abstract class Usuario   
+    {
+        public string strCodigo { get; set; }
+        public string strNombre { get; set; }
+        public string strCorreo { get; set; }
+        public Usuario(string strCodigo, string strNombre, string strCorreo)
+        {
+            this.strCodigo = strCodigo;
+            this.strNombre = strNombre;
+            this.strCorreo = strCorreo;
+        }
+        public abstract string obtenerRol();
+    }
+}
